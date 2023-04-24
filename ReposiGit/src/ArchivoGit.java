b70d65ea5e899ff2bfb126b3b0c575fa53410c73
MontserratSuarez <131239977+MontserratSuarez@users.noreply.github.com>
2023-04-24 12:29:00 -0600

    Add files via upload

---
+++ ReposiGit/src/ArchivoGit.java
@@ -0,0 +1,13 @@
+
+public class ArchivoGit {
+		  public static void main(String[] args) {
+		    System.out.println("Mi nombre es Montse");
+		    System.out.println("Tengo 20 años");
+		   
+		  }
+
+	public ArchivoGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
